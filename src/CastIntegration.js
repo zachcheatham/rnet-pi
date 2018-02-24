a5a1b53a17f245a6700cdc87bd658543fcf8a362
Zach Cheatham <zachcheatham@gmail.com>
2018-02-24 17:24:57 -0600

    Cast monitor patch.

--- src/CastIntegration.js
+++ src/CastIntegration.js
@@ -1,5 +1,5 @@
-const CastDeviceMonitor = require("castv2-device-monitor").DeviceMonitor
 const Source = require("./rnet/source");
+const patchCastMonitor = require("./util/patch").castMonitor;
 
 class CaseIntegration {
     constructor(rNet, config) {
@@ -38,82 +38,86 @@ class CaseIntegration {
     }
 
     start() {
-        for (let i in this._castDevices) {
-            let device = this._castDevices[i];
-            device.monitor = new CastDeviceMonitor(device.name);
-            device.lastState = false;
-            device.triggeredZones = false;
+        patchCastMonitor(() => {
+            const CastDeviceMonitor = require("castv2-device-monitor").DeviceMonitor
 
-            device.monitor.on("powerState", (stateName) => {
-                let powered = stateName == "on";
-                if (powered != device.lastState) {
-                    console.info("[Cast] \"%s\" power set to %s", device.name, powered);
-                    // Cast powered on
-                    if (powered) {
-                        // Interrupt the timer for power down
-                        if ("idleTimer" in device) {
-                            clearTimeout(device.idleTimer);
-                            delete device.idleTimer;
-                        }
+            for (let i in this._castDevices) {
+                let device = this._castDevices[i];
+                device.monitor = new CastDeviceMonitor(device.name);
+                device.lastState = false;
+                device.triggeredZones = false;
 
-                        // Only turn on trigger zones if no other zone is playing it
-                        if ("triggerZones" in device && !this.rNet.zonePlayingSource(device.sourceID)) {
-                            // Turn on the default zones
-                            for (i in device.triggerZones) {
-                                let zone = this.rNet.getZone(device.triggerZones[i][0], device.triggerZones[i][1]);
-                                zone.setPower(true);
+                device.monitor.on("powerState", (stateName) => {
+                    let powered = stateName == "on";
+                    if (powered != device.lastState) {
+                        console.info("[Cast] \"%s\" power set to %s", device.name, powered);
+                        // Cast powered on
+                        if (powered) {
+                            // Interrupt the timer for power down
+                            if ("idleTimer" in device) {
+                                clearTimeout(device.idleTimer);
+                                delete device.idleTimer;
                             }
-                            device.triggeredZones = true;
-                        }
-                    }
-                    // Cast powered off
-                    else {
-                        let source = this.rNet.getSource(device.sourceID);
-                        source.setDescriptiveText(null);
 
-                        if (device.triggeredZones) {
-                            // Wait the configured time to shut off zones
-                            device.idleTimer = setTimeout(() => {
-                                // Shut off all zones running the cast source
-                                for (let zone in this.rNet.getZonesPlayingSource)
-                                for (let ctrllrID = 0; ctrllrID < this.rNet.getControllersSize(); ctrllrID++) {
-                                    for (let zoneID = 0; zoneID < this.rNet.getZonesSize(ctrllrID); zoneID++) {
-                                        let zone = this.rNet.getZone(ctrllrID, zoneID);
-                                        if (zone != null && zone.getSourceID() == device.sourceID) {
-                                            zone.setPower(false);
+                            // Only turn on trigger zones if no other zone is playing it
+                            if ("triggerZones" in device && !this.rNet.zonePlayingSource(device.sourceID)) {
+                                // Turn on the default zones
+                                for (i in device.triggerZones) {
+                                    let zone = this.rNet.getZone(device.triggerZones[i][0], device.triggerZones[i][1]);
+                                    zone.setPower(true);
+                                }
+                                device.triggeredZones = true;
+                            }
+                        }
+                        // Cast powered off
+                        else {
+                            let source = this.rNet.getSource(device.sourceID);
+                            source.setDescriptiveText(null);
+
+                            if (device.triggeredZones) {
+                                // Wait the configured time to shut off zones
+                                device.idleTimer = setTimeout(() => {
+                                    // Shut off all zones running the cast source
+                                    for (let zone in this.rNet.getZonesPlayingSource)
+                                    for (let ctrllrID = 0; ctrllrID < this.rNet.getControllersSize(); ctrllrID++) {
+                                        for (let zoneID = 0; zoneID < this.rNet.getZonesSize(ctrllrID); zoneID++) {
+                                            let zone = this.rNet.getZone(ctrllrID, zoneID);
+                                            if (zone != null && zone.getSourceID() == device.sourceID) {
+                                                zone.setPower(false);
+                                            }
                                         }
                                     }
-                                }
-                                device.triggeredZones = false;
-                            }, device.idleTimeout);
+                                    device.triggeredZones = false;
+                                }, device.idleTimeout);
+                            }
                         }
                     }
-                }
-                device.lastState = powered;
-            })
-            .on("media", (media) => {
-                console.log("[Cast] \"%s\" is now playing %s by %s", device.name, media.title, media.artist);
+                    device.lastState = powered;
+                })
+                .on("media", (media) => {
+                    console.log("[Cast] \"%s\" is now playing %s by %s", device.name, media.title, media.artist);
 
-                // Only turn on trigger zones if no other zone is playing it
-                if (!this.rNet.zonePlayingSource(device.sourceID)) {
-                    // Turn on the default zones
-                    for (i in device.triggerZones) {
-                        let zone = this.rNet.getZone(device.triggerZones[i][0], device.triggerZones[i][1]);
-                        zone.setPower(true);
+                    // Only turn on trigger zones if no other zone is playing it
+                    if (!this.rNet.zonePlayingSource(device.sourceID)) {
+                        // Turn on the default zones
+                        for (i in device.triggerZones) {
+                            let zone = this.rNet.getZone(device.triggerZones[i][0], device.triggerZones[i][1]);
+                            zone.setPower(true);
+                        }
+                        device.triggeredZones = true;
                     }
-                    device.triggeredZones = true;
-                }
 
-                // TODO Temporary descriptive text of track
-            })
-            .on("application", (application) => {
-                console.log("[Cast] \"%s\" is now running %s", device.name, application);
-                let source = this.rNet.getSource(device.sourceID);
-                source.setDescriptiveText(application);
-            });
+                    // TODO Temporary descriptive text of track
+                })
+                .on("application", (application) => {
+                    console.log("[Cast] \"%s\" is now running %s", device.name, application);
+                    let source = this.rNet.getSource(device.sourceID);
+                    source.setDescriptiveText(application);
+                });
 
-            console.info("[Cast] Monitoring \"%s\"", device.name);
-        }
+                console.info("[Cast] Connected to \"%s\"", device.name);
+            }
+        });
     }
 
     stop() {
---
+++ src/util/patch.js
@@ -0,0 +1,18 @@
+const { exec } = require("child_process");
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
+
+module.exports = {
+    castMonitor: function(callback) {
+        if (os.type() != "Windows_NT")
+        {
+            const cwd = path.join(path.dirname(fs.realpathSync(__filename)), "/../..");
+            const options = {"cwd": cwd}
+
+            exec("patch -N -p1 < cast-monitor.patch", options, function(error, stdout, stderr) {
+                callback();
+            });
+        }
+    }
+}
